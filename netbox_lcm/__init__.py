9eb4dcf9ff337c2881716984d373168c409e8d91
Nate Reeves <nathan.a.reeves@gmail.com>
2025-09-05 19:22:56 +0800

    Update __init__.py
    Permit install on V4.4

--- netbox_lcm/__init__.py
+++ netbox_lcm/__init__.py
@@ -14,7 +14,7 @@ class NetBoxLifeCycle(PluginConfig):
     author_email = metadata.get('Author-email')
     base_url = 'lifecycle'
     min_version = '4.3.0'
-    max_version = '4.3.99'
+    max_version = '4.4.99'
     required_settings = []
     default_settings = {}
     queues = []
